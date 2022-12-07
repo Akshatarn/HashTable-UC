c108e88bf993ed2558375db6c5ebfe5d9260ef9b
akshata_rn <akshatarn123@gamail.com>
2022-12-07 12:38:32 +0530

    Frequency of word in paragraph is calculated

--- HashTable-UC/Program.cs
+++ HashTable-UC/Program.cs
@@ -11,9 +11,7 @@ namespace HashTable_UC
         public static void CountNumbOfOccurence(string paragraph)
         {
             MyNode<string, int> hashTabe = new MyNode<string, int>(6);
-
             string[] words = paragraph.Split(' ');
-
             foreach (string word in words)
             {
                 if (hashTabe.Exists(word.ToLower()))
@@ -23,21 +21,35 @@ namespace HashTable_UC
             }
             Console.WriteLine("Please find below words with its frequency of occurrence's :- ");
             hashTabe.Display();
+            Console.WriteLine();
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Demo Program on HashTables in DSA .\n");
             Console.WriteLine("Please sslect any one from below given options :\n" +
-                "1 : Checking frequency of occurence for a single line statement (UC1).\n");
+                "1 : Checking frequency of occurence of words in a single line statement (UC1).\n" +
+                "2 : Checking frequency of occurence of words in a paragraph (UC2).\n");
             int option = Convert.ToInt32((string)Console.ReadLine());
             switch (option)
             {
                 case 1:
-                    string paragraph = "To be or not to be";
+                    string paragraph = "To be or not to be";//UC1 frequency of occurrence of words in a sentence .
                     CountNumbOfOccurence(paragraph);
+                    Console.WriteLine();
+                    break;
+                case 2:                     //UC2
+                    string paragraph1 = "Paranoids are not paranoid " +
+                        "because they are paranoid but " +
+                        "because they keep putting themselves " +
+                        "deliberately into paranoid avoidable " +
+                        "situations";
+                    CountNumbOfOccurence(paragraph1);
+                    break;
+                default:
+                    Console.WriteLine("Invalid option selected ! Please try again .");
                     break;
             }
-
         }
     }
 }
+
